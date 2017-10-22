53ab9823536d2bc87e90c0e1499541bc9c026542
Hari <coolhari1996@gmail.com>
2017-10-22 12:14:04 +0530

    Backedn File to add inventory order request

---
+++ Codes/ManageGuru_Web_Management/php/addinventoryorder.php
@@ -0,0 +1,18 @@
+<?php
+	include "connect.php";
+	extract($_POST);
+	$sql = "SELECT * FROM inventory_orders WHERE item_name='$item_name' and item_type='$item_type'";
+	$result = $conn->query($sql);
+	if($result->num_rows > 0)
+	{
+		$row = $result->fetch_assoc();
+		$sql = "UPDATE inventory_orders SET quantity=".$row['quantity']."+$quantity WHERE item_type='$item_type' and item_name='$item_name'";
+		$result = $conn->query($sql);
+		echo json_encode(array("item_name"=>$item_name,"item_type"=>$item_type,"quant"=>$row['quantity']+$quantity));		
+	}
+	else
+	{
+		$sql = "INSERT INTO inventory_orders(item_type,item_name,quantity) VALUES ('$item_type','$item_name',$quantity)";
+		$result = $conn->query($sql);
+		echo json_encode(array("item_name"=>$item_name,"item_type"=>$item_type,"quant"=>$quantity));	
+	}
